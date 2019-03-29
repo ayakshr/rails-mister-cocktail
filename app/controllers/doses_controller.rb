13314091b80ecf099018e62311e0b995297b16cd
Aya Kashihara <aya.kashihara@gmail.com>
2019-03-29 17:08:57 +0900

    show page updated

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,7 +1,6 @@
 class DosesController < ApplicationController
   def new
     @cocktail = Cocktail.find(params[:cocktail_id])
-    @ingredient = Ingredient.find(params[:ingredient_id])
     @dose = Dose.new
   end
 
